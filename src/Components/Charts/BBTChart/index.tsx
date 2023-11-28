dbc9c0bdef6734b39e0dfe3515fdbc494eeb6288
Anurag Yadav <anurag@mithyalabs.com>
2023-11-28 23:04:40 +0530

    BBT Chart

---
+++ src/Components/Charts/BBTChart/index.tsx
@@ -0,0 +1,125 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React, {FC} from 'react';
+import {LineChart, yAxisSides} from 'react-native-gifted-charts';
+import {
+  LineChartPropsType,
+  itemType,
+} from 'react-native-gifted-charts/src/LineChart/types';
+
+export interface IBBTData {
+  day: string;
+  date: number;
+  temperature: number;
+}
+
+export interface IBBTChart extends Omit<LineChartPropsType, 'data'> {
+  data: Array<Omit<itemType, 'value'> & IBBTData & {value?: number}>;
+}
+
+const BBTChart: FC<IBBTChart> = ({
+  yAxisSide = yAxisSides.RIGHT,
+  ...chartProps
+}) => {
+  const data: LineChartPropsType['data'] = (chartProps?.data ?? [])?.map(
+    ({date, day, temperature, ...lineChartData}) => ({
+      ...lineChartData,
+      value: temperature,
+      dataPointText: `${temperature}`,
+      focusedDataPointLabelComponent: () => CustomDataPointLabel(temperature),
+      focusedCustomDataPoint: () => FocusedCustomDataPoint(),
+      labelComponent: () => CustomXAxisLabel(day, date),
+    }),
+  );
+  return (
+    <LineChart
+      {...chartProps}
+      color="#14214e"
+      customDataPoint={CustomDataPoint}
+      data={data}
+      dashGap={0}
+      maxValue={4}
+      noOfSections={8}
+      roundToDigits={2}
+      rulesColor="#e9f5f6"
+      showFractionalValues
+      spacing={45}
+      stepHeight={50}
+      stepValue={0.5}
+      focusEnabled
+      //   showStripOnFocus
+      showTextOnFocus
+      //   stripHeight={height}
+      stripWidth={65}
+      stripColor="#eaf9f2"
+      stripOpacity={0.5}
+      xAxisColor="#e9f5f6"
+      yAxisLabelWidth={45}
+      width={300}
+      yAxisOffset={96}
+      yAxisSide={yAxisSide}
+      yAxisThickness={0}
+      xAxisLabelsVerticalShift={50}
+      xAxisLabelsHeight={40}
+    />
+  );
+};
+
+const CustomDataPoint = () => <View style={styles.customDataPoint} />;
+
+const FocusedCustomDataPoint = () => (
+  <View style={styles.focusedCustomDataPoint} />
+);
+
+const CustomDataPointLabel = (dataPoint: number) => (
+  <View style={styles.customDataPointLabel}>
+    <Text style={styles.dataPoint}>{dataPoint}</Text>
+  </View>
+);
+
+const CustomXAxisLabel = (day: string, date: number) => (
+  <View style={styles.customXAxisLabel}>
+    <Text style={styles.day}>{day}</Text>
+    <Text style={styles.date}>{date}</Text>
+  </View>
+);
+
+export default BBTChart;
+
+const styles = StyleSheet.create({
+  customDataPoint: {
+    backgroundColor: 'white',
+    borderColor: '#14214e',
+    borderRadius: 9999,
+    borderWidth: 3,
+    height: 15,
+    width: 15,
+  },
+  focusedCustomDataPoint: {
+    backgroundColor: '#14214e',
+    borderRadius: 9999,
+    height: 20,
+    width: 20,
+  },
+  customDataPointLabel: {
+    marginTop: 35,
+    marginLeft: 10,
+  },
+  dataPoint: {
+    fontSize: 13,
+    color: '#14214e',
+  },
+  customXAxisLabel: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 5,
+  },
+  day: {
+    color: '#14214e',
+    fontWeight: 'bold',
+  },
+  date: {
+    color: '#14214e',
+  },
+});
--- src/Screens/Charts/index.tsx
+++ src/Screens/Charts/index.tsx
@@ -1,57 +1,19 @@
-import {StyleSheet, Text, View, useWindowDimensions} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import React, {FC} from 'react';
-import {LineChart, yAxisSides} from 'react-native-gifted-charts';
+import BBTChart from '../../Components/Charts/BBTChart';
+import useMockData from '../../Hooks/useMockData';
 
 export interface IChartsProps {}
 
 const Charts: FC = () => {
-  const {height, width} = useWindowDimensions();
-  const data = [
-    {label: 'S', value: 97.1},
-    {label: 'M', value: 97.3},
-    {label: 'T', value: 97.4},
-    {label: 'W', value: 97.5},
-    {label: 'T', value: 97.6},
-    {label: 'F', value: 97.7},
-    {label: 'S', value: 97.5},
-  ];
+  const data = useMockData('BBTChart');
 
   return (
     <View style={styles.root}>
-      <Text>
-        <LineChart
-          color="#14214e"
-          customDataPoint={CustomDataPoint}
-          dashGap={0}
-          data={data}
-          maxValue={4}
-          noOfSections={8}
-          roundToDigits={2}
-          rulesColor="#e9f5f6"
-          showFractionalValues
-          spacing={45}
-          stepHeight={70}
-          stepValue={0.5}
-          focusEnabled
-          showStripOnFocus
-          showTextOnFocus
-          stripHeight={height}
-          stripWidth={65}
-          stripColor="#eaf9f2"
-          stripOpacity={0.5}
-          width={width - 90}
-          xAxisColor="#e9f5f6"
-          yAxisLabelWidth={45}
-          yAxisOffset={96}
-          yAxisSide={yAxisSides.RIGHT}
-          yAxisThickness={0}
-        />
-      </Text>
+      <BBTChart data={data} />
     </View>
   );
 };
-
-const CustomDataPoint = () => <View style={styles.customDataPoint} />;
 
 export default Charts;
 
@@ -59,13 +21,5 @@ const styles = StyleSheet.create({
   root: {
     paddingHorizontal: 15,
     backgroundColor: 'white',
-  },
-  customDataPoint: {
-    backgroundColor: 'white',
-    borderColor: '#14214e',
-    borderRadius: 9999,
-    borderWidth: 3,
-    height: 12,
-    width: 12,
   },
 });
